013be223ce717ca7219c667e03deb25e682cd232
JGProg <jessy.giacomoni@me.com>
2013-10-29 18:39:54 +0100

    Add new main for exe3

---
+++ exo3/main.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* methods pour résoudre l'exercice */
+void caculMatriciel(void);
+
+void * ecrireTampon(int * tampon);
+void * lireTampon(int * tampon);
+
+int main ()
+{
+	calculmatriciel();
+	return 1;
+}
+
+void calculmatriciel(void)
+{
+    int firstMatrice [2];
+    int secondMatrice [2];
+    
+    
+    pthread_exit(NULL);
+    
+    
+}
+
+void* ecrireTampon(int* tampon)
+{
+	
+	pthread_exit(NULL);
+}
+
+void* lireTampon(int* tampon)
+{
+	pthread_exit(NULL);
+}
